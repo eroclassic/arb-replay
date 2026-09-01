b5ae2ca512cb8ccaa2f3d20d431d02fb564d3b50
hunter nguyen <sigre7944@gmail.com>
2026-09-01 21:29:44 +0100

    feat: add market

---
+++ include/arbreplay/market.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "arbreplay/outcome_book.hpp"
+#include <arbreplay/outcome_id.hpp>
+
+namespace arbreplay {
+class Market {
+public:
+  [[nodiscard]] bool empty() const noexcept { return outcomes_.empty(); }
+
+  [[nodiscard]] size_t size() const noexcept { return outcomes_.size(); }
+
+  [[nodiscard]] OutcomeBook *find_outcome(const OutcomeId &id) {
+    auto iterator = outcomes_.find(id);
+
+    if (iterator == outcomes_.end()) {
+      return nullptr;
+    }
+
+    return &iterator->second;
+  }
+
+  [[nodiscard]] const OutcomeBook *find_outcome(const OutcomeId &id) const {
+    auto iterator = outcomes_.find(id);
+
+    if (iterator == outcomes_.end()) {
+      return nullptr;
+    }
+
+    return &iterator->second;
+  }
+
+  bool add_outcome(const OutcomeId id) {
+    return outcomes_.try_emplace(std::move(id)).second;
+  }
+
+private:
+  std::map<OutcomeId, OutcomeBook> outcomes_;
+};
+
+} // namespace arbreplay
